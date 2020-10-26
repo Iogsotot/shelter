8cf0f097da77a4bfc17e69c77782d2d6b1181d0e
Iogsotot <ajustusa@gmail.com>
2020-10-26 14:56:54 +0300

    WIP: popup

--- pages/main/popup.js
+++ pages/main/popup.js
@@ -1,21 +1,62 @@
-let pets = [];
-let fullPetsList = [];
-let popupTitle = 
-let popupSubtitle =
+// let pets = [];
+// let fullPetsList = [];
+let popup = document.querySelector('.popup');
+let slide = document.querySelector('.slide');
+// let title = document.querySelector('.');
+// let subtitle = document.querySelector('.');
+// let text = document.querySelector('.');
+// let age = document.querySelector('.');
+// let age = document.querySelector('.');
+// let age = document.querySelector('.');
+// let age = document.querySelector('.');
+// let img = document.querySelector('.');
 
-const request = new XMLHttpRequest();
-request.open('GET', './pets.json');
+let cancelBtn = document.querySelector('.cancel__btn');
 
-request.onload = () => {
-    pets = JSON.parse(request.response);
-
-
-    for (let i = 0; i < 6; i++) {                   // размножаем животных
-        fullPetsList = [...fullPetsList, ...pets];  //добавляем к массиву FullPetsList новых животных
-
-    }
-    // createPets();
+function showPopup () {
+    // onClickClose(popup)
+    popup.style.zIndex = '1';
+    popup.classList.remove('hide');
+    console.log('я тоже работаю');
+}
+ 
+function hidePopup () {
+    popup.classList.add('hide');
+    // popup.style.zIndex = '-100';
+    setTimeout( () => {popup.style.zIndex = '-100'}, 250);          //даём время анимации
+    console.log('я работаю');
 }
 
+function onClickClose(elem) {                             // вызвать в момент показа окна, где elem - окно
+    function outsideClickListener(event) {
+        if (!elem.contains(event.target)) {             // проверяем, что клик не по элементу
+            hidePopup();
+            document.removeEventListener('click', outsideClickListener);
+        }               
+    }
+    document.addEventListener('click', outsideClickListener)
+}
 
-response.send()
+//listeners
+cancelBtn.addEventListener('click', hidePopup);
+slide.addEventListener('click', showPopup);
+
+//run
+hidePopup()
+
+// const request = new XMLHttpRequest();
+// request.open('GET', './pets.json');
+
+// request.onload = () => {
+//     pets = JSON.parse(request.response);
+
+
+//     for (let i = 0; i < 6; i++) {                   // размножаем животных
+//         fullPetsList = [...fullPetsList, ...pets];  //добавляем к массиву FullPetsList новых животных
+
+//     }
+//     // createPets();
+// }
+
+
+// response.send()
